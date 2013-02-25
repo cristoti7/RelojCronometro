2d033c388bace52668406864b9045f43d7aa2ef1
Cristopher Alejandro Aranda Garcia <cristoti7@hotmail.com>
2013-02-24 18:08:29 -0600

    Reloj y cronometro funcionando sin stop

--- RelojCronometro/RelojCronometro/Cronometro.cs
+++ RelojCronometro/RelojCronometro/Cronometro.cs
@@ -7,6 +7,24 @@ namespace RelojCronometro
 		public Cronometro ()
 		{
 		}
+		
+		public void inicializar ()
+		{
+			int horas = 0, minutos = 0, segundos = 0;
+			Console.WriteLine ("Cronometro inicializado");
+
+				for (horas=0; horas<24; horas++) {
+					for (minutos=0; minutos<60; minutos++) {
+						for (segundos=0; segundos<60; segundos++) {
+							Console.WriteLine ("{0}:{1}:{2}", horas, minutos, segundos);
+							System.Threading.Thread.Sleep (1000);
+							Console.Clear ();
+                             
+						}
+					}
+				}
+			}
+		}
 	}
-}
+
 
--- RelojCronometro/RelojCronometro/Reloj.cs
+++ RelojCronometro/RelojCronometro/Reloj.cs
@@ -3,10 +3,51 @@ using System;
 namespace RelojCronometro
 {
 	public class Reloj
+{
+	public Reloj ()
 	{
-		public Reloj ()
-		{
-		}
 	}
+	
+	public void inicializar ()
+		{
+		
+			Console.WriteLine ("Programa de un Reloj");
+			int h = DateTime.Now.Hour;
+			int m = DateTime.Now.Minute;
+			int s = DateTime.Now.Second;
+			int x;
+			while (true) {
+				Console.WriteLine ("La hora actual  es  {0}:{1}:{2}", h, m, s);
+				
+				x = Environment.TickCount;
+				
+				while (Environment.TickCount <= x + 1000)
+					;
+				s = s + 1;
+				
+				if (s == 60) {
+					s = 0;
+					m = m + 1;
+					if (m == 60) {
+						m = 0;
+						h = h + 1;
+						if (h == 24) {
+							h = 0;
+							
+						}
+					}
+				}
+			}
+			
+			
+			
+			
+			
+			
+			
+		}
+			
+
+}
 }
 
